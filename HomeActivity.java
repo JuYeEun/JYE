c00b935d79dce5059b4afaa33aa534705be897f2
JuYeEun <107421819+JuYeEun@users.noreply.github.com>
2022-07-21 17:48:12 +0900

    Add files via upload
    Splash, HomeActivity

---
+++ HomeActivity.java
@@ -0,0 +1,42 @@
+package com.cookandroid.jye;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button bt1, bt2, bt3;
+    SearchView searchView;
+    Fragment fragment;
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homeactivity);
+
+        bt1 = (Button) findViewById(R.id.btn1);
+        bt2 = (Button) findViewById(R.id.btn2);
+        bt3 = (Button) findViewById(R.id.btn3);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
---
+++ SplashActivity.java
@@ -0,0 +1,27 @@
+package com.cookandroid.jye;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
